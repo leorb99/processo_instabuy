237b48b7d2edff146795fcfedb37a8b494679262
leorb99 <leo.rb1999@gmail.com>
2025-12-16 23:42:39 -0300

    refactor api

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -4,44 +4,38 @@ import 'package:flutter_application_instabuy/models/products.dart' as products;
 import 'package:flutter_application_instabuy/models/promos.dart' as promos;
 import 'package:http/http.dart' as http;
 
-class BannerApi {
-  Future<List<banners.Welcome?>> getAllBanners() async {
+var responseJson;
+
+class Api {
+  Future<void> fetchData() async {
     var uri = Uri.parse('https://api.instabuy.com.br/apiv3/layout')
       .replace(queryParameters: {'subdomain': 'bigboxdelivery'});
     var response = await http.get(uri);
     if (response.statusCode == 200) {
-      final responseJson = json.decode(const Utf8Decoder().convert(response.bodyBytes)); 
-      final bnrs = json.encode(responseJson['data']['banners']);
-      return banners.welcomeBannersFromJson(bnrs);
+      responseJson = json.decode(const Utf8Decoder().convert(response.bodyBytes));
+    } else {
+      throw Exception('Erro na requisição da API!');  
     }
-    return Future.error('Erro na requisição dos banners!');
+  } 
+}
+
+class BannerApi {
+  List<banners.Welcome?> getAllBanners() {
+    final banner = json.encode(responseJson['data']['banners']);
+    return banners.welcomeBannersFromJson(banner);
   }
 }
 
 class PromoApi {
-  Future<List<promos.Welcome?>> getAllPromos() async {
-    var uri = Uri.parse('https://api.instabuy.com.br/apiv3/layout')
-      .replace(queryParameters: {'subdomain': 'bigboxdelivery'});
-    var response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final responseJson = json.decode(const Utf8Decoder().convert(response.bodyBytes)); 
+  List<promos.Welcome?> getAllPromos() {
       final promo = json.encode(responseJson['data']['promo']);
       return promos.welcomePromosFromJson(promo);
-    }
-    return Future.error('Erro na requisição das promos!');
   }
 }
 
 class ProductsApi {
-  Future<List<products.Welcome?>> getAllProducts() async {
-    var uri = Uri.parse('https://api.instabuy.com.br/apiv3/layout')
-      .replace(queryParameters: {'subdomain': 'bigboxdelivery'});
-    var response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final responseJson = json.decode(const Utf8Decoder().convert(response.bodyBytes)); 
+  List<products.Welcome?> getAllProducts() {
       final prods = json.encode(responseJson['data']['collection_items']);
       return products.welcomeProductsFromJson(prods);
-    }
-    return Future.error('Erro na requisição dos produtos!');
   }
 }
